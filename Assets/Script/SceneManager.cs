923fff89daf7be05833d8c177d6377ca82a487f0
PatheticDart <114214218+PatheticDart@users.noreply.github.com>
2024-03-10 23:57:54 +0800

    2D GAME

---
+++ Assets/Script/SceneManager.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SceneManager : MonoBehaviour
+{
+    public GameObject[] Scenes;
+    private int armorPoints = 10070, activeScene;
+    public TextMeshProUGUI armorValue;
+    public GameObject Stats;
+    public bool managerRunning = true;
+
+    void Start()
+    {
+        Scenes[0].SetActive(true);
+        Stats.SetActive(false);
+    }
+
+    void Update()
+    {
+        armorValue.text = armorPoints.ToString();
+        SceneChanger();
+    }
+
+    public void SceneChanger()
+    {
+        if(managerRunning == true)
+        {
+            for (int i = 0; i < Scenes.Length; i++)
+            {
+                Scenes[i].SetActive(i == activeScene);
+            }
+        }
+        
+    }
+
+    public void NextScene()
+    {
+        activeScene++;
+    }
+
+    public void StartButton()
+    {
+        activeScene = 1;
+    }
+
+    public void RepairKit()
+    {
+        armorPoints += 6000;
+    }
+
+    public void ToggleStatsOn()
+    {
+        Stats.SetActive(true);
+    }
+
+    public void ToggleStatsOff()
+    {
+        Stats.SetActive(false);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void PlayerDamage20()
+    {
+        armorPoints -= 2014;
+    }
+
+    public void PlayerDamage10()
+    {
+        armorPoints -= 1007;
+    }
+
+    public void PlayerDamage5()
+    {
+        armorPoints -= 504;
+    }
+
+    public void PauseManager()
+    {
+        managerRunning = false;
+    }
+
+    public void ResumeManager()
+    {
+        managerRunning = true;
+    }
+}
